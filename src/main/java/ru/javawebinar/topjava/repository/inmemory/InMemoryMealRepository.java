1be6b6f8c18aa37cf69dce7220d82815effcf1a6
artifiner <askovin@yandex.ru>
2020-06-14 16:07:54 +0300

    Add userId in methods signatures (remove SecurityUtil usage from service and repository layers)

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -12,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
@@ -27,24 +29,24 @@ public class InMemoryMealRepository implements MealRepository {
             repository.put(meal.getId(), meal);
             return meal;
         }
-        return get(meal.getId()) != null ? repository.replace(meal.getId(), meal) : null;
+        return get(meal.getId(), meal.getUserId()) != null ? repository.replace(meal.getId(), meal) : null;
     }
 
     @Override
-    public boolean delete(int id) {
-        return get(id) != null && repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        return get(id, userId) != null && repository.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) {
         Meal meal = repository.get(id);
-        return meal.getUserId() == SecurityUtil.authUserId() ? meal : null;
+        return meal.getUserId() == userId ? meal : null;
     }
 
     @Override
-    public List<Meal> getAll() {
+    public List<Meal> getAll(int userId) {
         return repository.values().stream()
-                .filter(meal -> meal.getUserId() == SecurityUtil.authUserId())
+                .filter(meal -> meal.getUserId() == userId)
                 .sorted((meal1, meal2) -> meal2.getDate().compareTo(meal1.getDate()))
                 .collect(Collectors.toList());
     }
