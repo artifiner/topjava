57e97f3996567a0b5043b570949cd1a21f635472
artifiner <askovin@yandex.ru>
2020-06-15 19:40:51 +0300

    Add date filter into repository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,9 +3,13 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -45,8 +49,18 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getAll(int userId) {
+        return getFilteredByDate(userId, LocalDate.MIN, LocalDate.MAX);
+    }
+
+    @Override
+    public List<Meal> getFilteredByDate(int userId, LocalDate startDate, LocalDate endDate) {
         return repository.values().stream()
                 .filter(meal -> meal.getUserId() == userId)
+                .filter(meal -> DateTimeUtil.isBetweenHalfOpen(
+                        meal.getDateTime(),
+                        startDate.atTime(LocalTime.MIN),
+                        endDate.atTime(LocalTime.MAX)
+                ))
                 .sorted((meal1, meal2) -> meal2.getDate().compareTo(meal1.getDate()))
                 .collect(Collectors.toList());
     }
