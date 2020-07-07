5afc8d767401dfa87b92990646ed7f37b38c5ae3
artifiner <askovin@yandex.ru>
2020-07-08 00:11:51 +0300

    Implement persistence profiles

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -13,17 +13,19 @@ public class Profiles {
             HSQL_DB = "hsqldb";
 
     //  Get DB profile depending of DB driver in classpath
-    public static String getActiveDbProfile() {
+    public static String[] getActiveDbProfiles() {
+        String dataBase;
         try {
             Class.forName("org.postgresql.Driver");
-            return POSTGRES_DB;
+            dataBase = POSTGRES_DB;
         } catch (ClassNotFoundException ex) {
             try {
                 Class.forName("org.hsqldb.jdbcDriver");
-                return Profiles.HSQL_DB;
+                dataBase = HSQL_DB;
             } catch (ClassNotFoundException e) {
                 throw new IllegalStateException("Could not find DB driver");
             }
         }
+        return new String[]{REPOSITORY_IMPLEMENTATION, dataBase};
     }
 }
