ef23d51ebe19dd7a976e67c0f2ffdfc616341cf0
artifiner <askovin@yandex.ru>
2020-08-11 21:15:11 +0300

    Implement users update

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.model;
 
 import org.springframework.util.Assert;
+import ru.javawebinar.topjava.HasId;
 
 import org.hibernate.Hibernate;
 
@@ -10,7 +11,7 @@ import javax.persistence.*;
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
 //@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
-public abstract class AbstractBaseEntity {
+public abstract class AbstractBaseEntity implements HasId {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -29,6 +30,7 @@ public abstract class AbstractBaseEntity {
         this.id = id;
     }
 
+    @Override
     public void setId(Integer id) {
         this.id = id;
     }
@@ -41,10 +43,6 @@ public abstract class AbstractBaseEntity {
     public int id() {
         Assert.notNull(id, "Entity must has id");
         return id;
-    }
-
-    public boolean isNew() {
-        return this.id == null;
     }
 
     @Override
